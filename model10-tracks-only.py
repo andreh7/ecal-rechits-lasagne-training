bd898d280287f1e2627b1e081b52d9a2db3ae089
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-09-26 14:47:44 +0200

    moved some functions and binning parameters to a new file trackmodelutils.py

--- model10-tracks-only.py
+++ model10-tracks-only.py
@@ -13,6 +13,8 @@ import numpy as np
 import theano.tensor as T
 import math
 
+from trackmodelutils import myArange, makeSymmetricBinning, trkBinningDr
+
 #----------------------------------------------------------------------
 # model
 #----------------------------------------------------------------------
@@ -28,45 +30,6 @@ batchesPerSuperBatch = math.floor(3345197 / batchSize)
 #----------------------------------------------------------------------
 # function to prepare input data samples
 #----------------------------------------------------------------------
-
-# parameters of track histogram
-trkAbsDetaMax = 0.4
-trkAbsDphiMax = 0.4
-
-trkDetaBinWidth, trkDphiBinWidth = 2 * math.pi / 360.0, 2 * math.pi / 360.0
-
-# for radial histogram
-trkDrMax = 0.4
-trkDrBinWidth = 2 * math.pi /360.0
-
-#----------------------------------------
-
-# like numpy.arange but including the upper end
-def myArange(start, stop, step):
-    value = start
-    while True:
-        yield value
-        
-        if value >= stop:
-            break
-
-        value += step
-
-#----------------------------------------
-
-def makeSymmetricBinning(maxVal, step):
-    bins = list(myArange(0, maxVal, step))
-
-    # symmetrize
-    # but avoid doubling the zero value
-    return [ -x for x in bins[::-1][:-1]] + bins
-
-    
-#----------------------------------------
-
-# note that we do NOT need makeSymmetricBinning(..) here
-# because dr does not go negative
-trkBinningDr = list(myArange(0, trkDrMax, trkDrBinWidth))
 
 def makeTrackHistograms(dataset, rowIndices):
     # fills tracks into a histogram
---
+++ trackmodelutils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import math
+
+#----------------------------------------------------------------------
+
+# parameters for track histograms
+trkAbsDetaMax = 0.4
+trkAbsDphiMax = 0.4
+
+trkDetaBinWidth, trkDphiBinWidth = 2 * math.pi / 360.0, 2 * math.pi / 360.0
+
+# for radial histogram
+trkDrMax = 0.4
+trkDrBinWidth = 2 * math.pi /360.0
+
+#----------------------------------------------------------------------
+
+# like numpy.arange but including the upper end
+def myArange(start, stop, step):
+    value = start
+    while True:
+        yield value
+        
+        if value >= stop:
+            break
+
+        value += step
+
+#----------------------------------------------------------------------
+
+def makeSymmetricBinning(maxVal, step):
+    bins = list(myArange(0, maxVal, step))
+
+    # symmetrize
+    # but avoid doubling the zero value
+    return [ -x for x in bins[::-1][:-1]] + bins
+
+#----------------------------------------------------------------------
+
+# note that we do NOT need makeSymmetricBinning(..) here
+# because dr does not go negative
+trkBinningDr = list(myArange(0, trkDrMax, trkDrBinWidth))
+    
