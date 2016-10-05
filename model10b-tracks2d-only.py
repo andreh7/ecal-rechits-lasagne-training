6011b88d4a2ac49f66071554602f302a4b1666f7
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-10-05 15:00:25 +0200

    initial commit: 2D model with tracks only

---
+++ model10b-tracks2d-only.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# like model06 but with dropout layer only applied
+# to the rechits variables, not the other (track iso)
+# variables
+
+
+from lasagne.layers import DenseLayer
+
+from lasagne.init import GlorotUniform
+from lasagne.nonlinearities import softmax
+
+import theano.tensor as T
+import math
+
+from trackmodelutils import makeTrackHistograms2D, make2DTracksHistogramModel
+
+#----------------------------------------------------------------------
+# model
+#----------------------------------------------------------------------
+
+# size of minibatch
+batchSize = 32
+
+# how many minibatches to unpack at a time
+# and to store in the GPU (to have fewer
+# data transfers to the GPU)
+batchesPerSuperBatch = math.floor(3345197 / batchSize)
+
+#----------------------------------------------------------------------
+# function to prepare input data samples
+#----------------------------------------------------------------------
+
+def makeInput(dataset, rowIndices, inputDataIsSparse):
+    assert inputDataIsSparse,"non-sparse input data is not supported"
+
+    return [ 
+        makeTrackHistograms2D(dataset, rowIndices, relptWeighted = True)
+        ]
+
+
+#----------------------------------------------------------------------
+
+def makeModel():
+
+    # note that we need several input variables here
+    # 3D tensor
+    inputVarTracks         = T.tensor4('tracks')
+
+    tracksModel = make2DTracksHistogramModel(inputVarTracks)
+
+    # output
+    network = DenseLayer(
+        tracksModel,
+        num_units = 2,  # need two class classification, seems not to work well with sigmoid
+        nonlinearity = softmax,
+        W = GlorotUniform(),
+        )
+
+    return [ inputVarTracks ], network
+
+#----------------------------------------------------------------------
