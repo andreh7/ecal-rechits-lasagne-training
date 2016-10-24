9046a028cd31b668523fce0f52dd304a55b87a45
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-10-24 14:13:14 +0200

    now taking a results directory to infer the number of epochs and training sample size and saving the plot

--- plotLearningRate.py
+++ plotLearningRate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # from sgdWithLearningRateDecay(..) in train01.py
+import glob, os, re
 
 import numpy as np
 #----------------------------------------------------------------------
@@ -20,13 +21,62 @@ class LearningRate:
 # main
 #----------------------------------------------------------------------
 
+import argparse
+
+parser = argparse.ArgumentParser(prog='plotLearningRate.py',
+                                 formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+                                 )
+
+parser.add_argument('resultDir',
+                    nargs = 1,
+                    default = None,
+                    metavar = "D",
+                    help='result directory to infer parameters from',
+                    )
+
+options = parser.parse_args()
+
+#----------------------------------------
+if options.resultDir == None:
+    print >> sys.stderr,"should specify the result directory"
+    sys.exit(1)
+
+# find number of epochs
+fnames = glob.glob(os.path.join(options.resultDir[0],"roc-data-train-*.npz"))
+
+if not fnames:
+    print >> sys.stderr,"no roc-data-train-*.npz files found in",options.resultDir
+    sys.exit(1)
+
+# find latest epoch
+maxEpoch = None
+maxEpochFile = None
+for fname in fnames:
+    mo = re.search("roc-data-train-(\d+).npz$", fname)
+    # 'MVA' does not match
+    if not mo:
+        continue
+
+    thisEpoch = int(mo.group(1),10)
+    if maxEpoch == None or thisEpoch > maxEpoch:
+        maxEpoch = thisEpoch
+        maxEpochFile = fname
+
+    
+assert maxEpoch != None
+
+# find number of training samples 
+data = np.load(maxEpochFile)
+numTrainSamples = len(data['label'])    
+
+#----------------------------------------
+
 learningRate = LearningRate(learningRate = 1e-3,
                   learningRateDecay = 1e-7)
 
-numEpochs = 324
+learningRate = np.frompyfunc(learningRate, 1, 1)
 
 batchSize = 32
-numTrainSamples = 3424581
 
 # round down
 iterationsPerEpoch = numTrainSamples / batchSize
@@ -34,16 +84,17 @@ iterationsPerEpoch = numTrainSamples / batchSize
 yvalues = []
 xvalues = []
 
-iternum = 0
-for epoch in range(1, numEpochs + 1):
+# we can't plot every iteration for large datasets
+# and epoch numbers
+# plot e.g. 1000 points 
 
-    xvalues.extend(np.linspace(epoch, epoch + 1,
-                               iterationsPerEpoch,
-                               endpoint = False))
-    
-    for i in range(iterationsPerEpoch):
-        yvalues.append(learningRate(iternum))
-        iternum += 1
+numPoints = 1001
+
+itervalues = np.linspace(0, maxEpoch * iterationsPerEpoch, numPoints)
+xvalues    = np.linspace(1, maxEpoch, numPoints)
+
+assert len(itervalues) == len(xvalues)
+yvalues = learningRate(itervalues)
 
 import pylab
 pylab.figure(facecolor='white')
@@ -52,6 +103,8 @@ pylab.plot(xvalues, yvalues)
 pylab.grid()
 pylab.xlabel('epoch number')
 pylab.ylabel('learning rate')
-pylab.savefig("/tmp/learning-rate-evolution.pdf")
-print "plotting done"
+
+outputFname = os.path.join(options.resultDir[0], "learning-rate-evolution.pdf")
+pylab.savefig(outputFname)
+print "saved figure to",outputFname
 pylab.show()
