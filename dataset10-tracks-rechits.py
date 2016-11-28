366396c25cd6c4f963508be28a47b6207dc3087b
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-11-28 14:09:48 +0100

    initial commit: model with rechits image and high level variables

---
+++ dataset10-tracks-rechits.py
@@ -0,0 +1,171 @@
+# datasets including chose and worst track isolation (2016-05-28)
+
+# same as dataset04-barrel.py but with newer dataset allowing for pt,eta reweighting
+datasetDir = '../data/2016-11-18-photon-et'
+
+isBarrel = True
+
+dataDesc = dict(
+    train_files = [ # datasetDir + '/GJet20to40_rechits-barrel-train.t7',
+                    datasetDir + '/GJet20toInf_rechits-barrel-train.t7',
+                    # datasetDir + '/GJet40toInf_rechits-barrel-train.t7',
+                    ],
+
+    test_files  = [ # datasetDir + '/GJet20to40_rechits-barrel-test.t7',
+                    datasetDir + '/GJet20toInf_rechits-barrel-test.t7',
+                    # datasetDir + '/GJet40toInf_rechits-barrel-test.t7'
+                    ],
+
+    inputDataIsSparse = True,
+
+    # if one specifies nothing (or nil), the full sizes
+    # from the input samples are taken
+    # 
+    # if one specifies values < 1 these are interpreted
+    # as fractions of the sample
+    # trsize, tesize = 10000, 1000
+    # trsize, tesize = 0.1, 0.1
+    # trsize, tesize = 0.01, 0.01
+    
+# limiting the size for the moment because
+# with the full set we ran out of memory after training
+# on the first epoch
+    trsize = 0.5, tesize = 0.5,
+
+    # trsize, tesize = 0.05, 0.05
+
+    # trsize, tesize = 100, 100
+
+    # DEBUG
+    # trsize = 0.01, tesize = 0.01,
+)
+
+#----------------------------------------------------------------------
+
+def datasetLoadFunction(fnames, size, cuda, isTraining, reweightPtEta = True):
+
+    data = None
+
+    totsize = 0
+
+    from datasetutils import makeRecHitsConcatenator, makeTracksConcatenator, CommonDataConcatenator, SimpleVariableConcatenator, getActualSize, PtEtaReweighter
+
+    commonData = CommonDataConcatenator()
+    recHits = makeRecHitsConcatenator()
+    trackVars = makeTracksConcatenator([ 'vtxDz' ])
+
+    # only apply pt/eta reweighting for training dataset
+    reweightPtEta = reweightPtEta and isTraining
+
+    if reweightPtEta:
+      # for reweighting (use reconstructed pt and eta)
+      ptEta = SimpleVariableConcatenator(['pt', 'eta'],
+                                         dict(pt = lambda loaded:  loaded['phoVars']['phoEt'],
+                                              eta = lambda loaded: loaded['phoIdInput']['scEta'])
+                                         )
+
+    # load all input files
+    for fname in fnames:
+  
+        print "reading",fname
+        loaded = torchio.read(fname)
+
+        #----------
+        # determine the size
+        #----------
+        thisSize = getActualSize(size, loaded)
+
+        totsize += thisSize
+
+        #----------
+        # combine common data
+        #----------
+        commonData.add(loaded, thisSize)
+
+        #----------
+        # combine rechits
+        #----------
+        recHits.add(loaded, thisSize)
+    
+        #----------
+        # track variables
+        #----------
+        trackVars.add(loaded, thisSize)
+
+        #----------
+        # pt/eta reweighting variables
+        #----------
+        if reweightPtEta:
+          ptEta.add(loaded, thisSize)
+  
+    # end of loop over input files
+
+    #----------
+    # reweight signal to have the same background shape
+    # using a 2D (pt, eta) histogram
+    #----------
+    if reweightPtEta:
+      ptEtaReweighter = PtEtaReweighter(ptEta.data['pt'][:,0],
+                                        ptEta.data['eta'][:,0],
+                                        commonData.data['labels'],
+                                        isBarrel)
+
+      scaleFactors = ptEtaReweighter.getSignalScaleFactors(ptEta.data['pt'][:,0],
+                                                           ptEta.data['eta'][:,0],
+                                                           commonData.data['labels'])
+      
+      # keep original weights
+      commonData.data['weightsBeforePtEtaReweighting'] = np.copy(commonData.data['weights'])
+
+
+      commonData.data['weights'] *= scaleFactors
+
+    #----------
+    # normalize event weights
+    #----------
+    commonData.normalizeSignalToBackgroundWeights()
+
+    # make average weight equal to one over the sample
+    commonData.normalizeWeights()
+
+    #----------
+    data = commonData.data
+  
+    # add rechits
+    data['rechits'] = recHits.data
+
+    #----------
+    # normalize track variables to zero mean and unit variance
+    #----------
+    # commented out for the moment
+    # trackVars.normalize()
+
+    #----------
+    # add track variables
+    #----------
+    data['tracks'] = trackVars.data
+
+    #----------
+    # cross check for pt/eta reweighting, dump some variables
+    #----------
+    if reweightPtEta:
+      outputName = "/tmp/pt-reweighting.npz"
+      np.savez(outputName,
+               pt = ptEta.data['pt'][:,0],
+               eta = ptEta.data['eta'][:,0],
+               weights = commonData.data['weights'],
+               labels = commonData.data['labels'],
+               scaleFactors = scaleFactors,
+               sigHistogram = ptEtaReweighter.sigHistogram,
+               bgHistogram = ptEtaReweighter.bgHistogram,
+               )
+      print "wrote pt/eta reweighting data to", outputName
+
+      # sys.exit(1)
+
+
+    assert totsize == data['rechits']['numRecHits'].shape[0]
+  
+    return data, totsize
+
+#----------------------------------------------------------------------
---
+++ model13-tracks-and-rechits.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+# like model06 but with dropout layer only applied
+# to the rechits variables, not the other (track iso)
+# variables
+
+
+from lasagne.layers import InputLayer, DenseLayer, ConcatLayer
+
+from lasagne.init import GlorotUniform
+from lasagne.nonlinearities import rectify, softmax
+
+import theano.tensor as T
+import math
+
+import trackmodelutils, rechitmodelutils
+
+#----------------------------------------------------------------------
+# hidden units, filter sizes for convolutional network
+nstates = [64,64,128]
+filtsize = 5
+poolsize = 2
+
+#----------------------------------------------------------------------
+# model
+#----------------------------------------------------------------------
+
+# size of minibatch
+batchSize = 32
+
+# how many minibatches to unpack at a time
+# and to store in the GPU (to have fewer
+# data transfers to the GPU)
+batchesPerSuperBatch = math.floor(3345197 / batchSize)
+
+numTracksToInclude = 5
+
+#----------------------------------------------------------------------
+# function to prepare input data samples
+#----------------------------------------------------------------------
+
+
+# input dimensions for rechits
+# width, height = 7, 23
+
+# we use the wide rechits window because
+# we also want the wide track window
+# (but we could actually restrict the rechits
+# to a narrow window and just let the outer
+# rechits be zero in the network input without shifting)
+width, height = 35, 35
+
+# one degree: ~ size of a crystal in the barrel
+trackBinWidth = 2 * math.pi / 360.0
+
+# make a binning with fixed bin size and given number of bins
+tracksBinningDeta = trackmodelutils.makeBinningFromWidthAndNumber(trackBinWidth, width)
+tracksBinningDphi = trackmodelutils.makeBinningFromWidthAndNumber(trackBinWidth, height)
+
+# the binning contains also the upper edge of the last bin
+assert len(tracksBinningDeta) == width + 1
+assert len(tracksBinningDphi) == height + 1
+
+# the center point of the rechits in the dataset is
+# at 18,18 in Torch coordinates (one based)
+# i.e. at 17,17 in python coordinates (zero based)
+# so we have (0..34) x (0..34) in python coordinates
+# 
+# we make the tracking such that 
+
+unpacker = rechitmodelutils.RecHitsUnpacker(
+    width = width,
+    height = height,
+
+    # for shifting 18,18 to 4,12
+    # recHitsXoffset = -18 + 4,
+    # recHitsYoffset = -18 + 12,
+    )
+
+# make a consistent binning for the tracks
+trackVarsMaker = trackmodelutils.TrackVarsMaker(numTracksToInclude)
+
+def makeInput(dataset, rowIndices, inputDataIsSparse):
+    assert inputDataIsSparse,"non-sparse input data is not supported"
+
+    return [ 
+        # unpack rechits
+        unpacker.unpack(dataset, rowIndices),
+        trackVarsMaker.make(dataset, rowIndices)
+        ]
+
+#----------------------------------------------------------------------
+
+def makeModel():
+    # see also model06b-trkiso.py
+
+    # make two input variables: one for rechits, one for tracks
+    #
+    inputVarRecHits        = T.tensor4('rechits')
+    inputVarTracks         = T.matrix('trackVars')
+
+    # 2D convolution layers require a dimension for the input channels
+    inputLayerRecHits = InputLayer(shape=(None, 1 , width, height),
+                                   input_var = inputVarRecHits,
+                                   name = 'rechits',
+                                   )
+
+    inputVarsTracks = InputLayer(shape=(None, trackVarsMaker.numVars),
+                                 input_var = inputVarTracks,
+                                 name = 'trackVars',
+                                 )
+
+    #----------
+    recHitsModel = rechitmodelutils.makeRecHitsModel(inputLayerRecHits, nstates[:2], filtsize, poolsize)
+
+    #----------
+    # combine nn output from convolutional layers for
+    # rechits with track isolation variables
+    #----------
+
+    network = ConcatLayer([ recHitsModel, inputVarsTracks ],
+                          axis = 1)
+
+    network = DenseLayer(
+        network,
+        num_units = nstates[2],
+        W = GlorotUniform(),
+        nonlinearity = rectify)
+
+    #----------
+    # output
+    #----------
+    network = DenseLayer(
+        network,
+        num_units = 2,  # need two class classification, seems not to work well with sigmoid
+        nonlinearity = softmax,
+        W = GlorotUniform(),
+        )
+
+    return [ inputVarRecHits, inputVarTracks ], network
+
+#----------------------------------------------------------------------
