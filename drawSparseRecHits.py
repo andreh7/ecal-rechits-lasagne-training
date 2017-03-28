db18aa0048fc61db7ea5b8939194fda51f2f2b87
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-03-28 17:54:37 +0200

    added support for drawing a summary image for the rechits

--- drawSparseRecHits.py
+++ drawSparseRecHits.py
@@ -47,6 +47,60 @@ def drawIndividual(photonIndices):
 
 
 #----------------------------------------------------------------------
+
+def drawSummary():
+    # draw a projection of all photon candidates, separated
+    # by label
+
+    for label in (0, 1):
+
+        print "projecting label",label
+
+        data = np.zeros((width, height))
+
+        numFound = 0
+
+        # torch coordinates
+        XX, YY = np.meshgrid(np.arange(width) + 1.5, np.arange(height) + 1.5)
+
+        for photonIndex in range(len(labels)):
+
+            if labels[photonIndex] != label:
+                continue
+
+            numFound += 1
+
+            # note that the torch indices are one based
+            # so we subtract one here
+            baseIndex = firstIndices[photonIndex] - 1
+
+            for i in range(numRecHits[photonIndex]):
+
+                xc = xcoords[baseIndex + i] - 1
+                yc = ycoords[baseIndex + i] - 1
+
+                assert xc >= 0
+                assert xc < width
+                assert yc >= 0
+                assert yc < height
+
+                data[yc, xc] += energies[baseIndex + i]
+
+            # end of loop over rechits of this photon
+
+        # end of loop over photon candidates
+
+        # plot the matrix
+        pylab.figure()
+        pylab.pcolor(XX, YY, data, cmap = pylab.cm.Blues)
+
+        pylab.title("label=" + str(label) + " (%d candidates)" % numFound)
+        pylab.grid()
+
+    # end of loop over labels
+
+
+#----------------------------------------------------------------------
 # main
 #----------------------------------------------------------------------
 
