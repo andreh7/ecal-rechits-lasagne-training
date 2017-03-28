d64aaa078a7228c45234e526e902cd6e3c866af5
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-03-28 17:27:13 +0200

    factored out a function drawIndividual(..)

--- drawSparseRecHits.py
+++ drawSparseRecHits.py
@@ -8,6 +8,45 @@ import pylab
 import numpy as np
 
 #----------------------------------------------------------------------
+
+def drawIndividual(photonIndices):
+
+    for photonIndex in photonIndices:
+        assert photonIndex >= 0 and photonIndex < numPhotons, "photon indices must be in the range 0..%d" % (numPhotons - 1)
+
+
+    for photonIndex in photonIndices:
+
+        data = np.zeros((width, height))
+
+        # torch coordinates
+        XX, YY = np.meshgrid(np.arange(width) + 1.5, np.arange(height) + 1.5)
+
+        # note that the torch indices are one based
+        # so we subtract one here
+        baseIndex = firstIndices[photonIndex] - 1
+
+        for i in range(numRecHits[photonIndex]):
+
+            xc = xcoords[baseIndex + i] - 1
+            yc = ycoords[baseIndex + i] - 1
+
+            assert xc >= 0
+            assert xc < width
+            assert yc >= 0
+            assert yc < height
+
+            data[yc, xc] = energies[baseIndex + i]
+
+        # plot the matrix
+        pylab.figure()
+        pylab.pcolor(XX, YY, data, cmap = pylab.cm.Blues)
+
+        pylab.title("index " + str(photonIndex) + " (label=" + str(labels[photonIndex]) + ")")
+        pylab.grid()
+
+
+#----------------------------------------------------------------------
 # main
 #----------------------------------------------------------------------
 
@@ -39,41 +78,12 @@ print "read",numPhotons,"photons"
 
 photonIndices = [ int(photonIndex) for photonIndex in ARGV ]
 
-for photonIndex in photonIndices:
-    assert photonIndex >= 0 and photonIndex < numPhotons, "photon indices must be in the range 0..%d" % (numPhotons - 1)
+if photonIndices == [ -1 ]:
+    # draw average of all photons, separated by label
+    drawSummary()
+else:
+    drawIndividual(photonIndices)
 
-
-for photonIndex in ARGV:
-    
-    photonIndex = int(photonIndex)
-
-    data = np.zeros((width, height))
-
-    # torch coordinates
-    XX, YY = np.meshgrid(np.arange(width) + 1.5, np.arange(height) + 1.5)
-
-    # note that the torch indices are one based
-    # so we subtract one here
-    baseIndex = firstIndices[photonIndex] - 1
-
-    for i in range(numRecHits[photonIndex]):
-
-        xc = xcoords[baseIndex + i] - 1
-        yc = ycoords[baseIndex + i] - 1
-
-        assert xc >= 0
-        assert xc < width
-        assert yc >= 0
-        assert yc < height
-        
-        data[yc, xc] = energies[baseIndex + i]
-
-    # plot the matrix
-    pylab.figure()
-    pylab.pcolor(XX, YY, data, cmap = pylab.cm.Blues)
-
-    pylab.title("index " + str(photonIndex) + " (label=" + str(labels[photonIndex]) + ")")
-    pylab.grid()
     
 
 pylab.show()
