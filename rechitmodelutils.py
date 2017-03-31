andreh7/ecal-rechits-lasagne-training

commit c468087c5d8052b88ed23b15408fb981987ddccf

in RecHitsUnpacker.unpack(..): moved dict lookups out of the loop

rechitmodelutils.py
@@ -27,6 +27,14 @@ class RecHitsUnpacker:
 
         recHits = np.zeros((batchSize, 1, self.width, self.height), dtype = 'float32')
 
+        # get pointers to these here in order to avoid string comparisons
+        # in the loops
+        indexOffsets = dataset['rechits']['firstIndex']
+        numRecHits   = dataset['rechits']['numRecHits']
+        rechitsX     = dataset['rechits']['x']
+        rechitsY     = dataset['rechits']['x']
+        rechitsE     = dataset['rechits']['energy']
+
         for i in range(batchSize):
 
             rowIndex = rowIndices[i]
@@ -34,17 +42,17 @@ class RecHitsUnpacker:
             # we do NOT subtract one because from 'firstIndex' because
             # these have been already converted in the class SparseConcatenator
             # in datasetutils.py
-            indexOffset = dataset['rechits']['firstIndex'][rowIndex]
+            indexOffset = indexOffsets[rowIndex]
 
-            for recHitIndex in range(dataset['rechits']['numRecHits'][rowIndex]):
+            for recHitIndex in range(numRecHits[rowIndex]):
 
                 # we subtract -1 from the coordinates because these are one based
                 # coordinates for Torch (and SparseConcatenator does NOT subtract this)
-                xx = dataset['rechits']['x'][indexOffset + recHitIndex] - 1 + self.recHitsXoffset
-                yy = dataset['rechits']['y'][indexOffset + recHitIndex] - 1 + self.recHitsYoffset
+                xx = rechitsX[indexOffset + recHitIndex] - 1 + self.recHitsXoffset
+                yy = rechitsY[indexOffset + recHitIndex] - 1 + self.recHitsYoffset
 
                 if xx >= 0 and xx < self.width and yy >= 0 and yy < self.height:
-                    recHits[i, 0, xx, yy] = dataset['rechits']['energy'][indexOffset + recHitIndex]
+                    recHits[i, 0, xx, yy] = rechitsE[indexOffset + recHitIndex]
 
             # end of loop over rechits of this photon
         # end of loop over minibatch indices

commit 1bf7b4e07dd90fb666127689947d26efdebcfce0

bugfix for bug introduced in c468087: was using x rechit coordinates for both x and y

rechitmodelutils.py
@@ -32,7 +32,7 @@ class RecHitsUnpacker:
         indexOffsets = dataset['rechits']['firstIndex']
         numRecHits   = dataset['rechits']['numRecHits']
         rechitsX     = dataset['rechits']['x']
-        rechitsY     = dataset['rechits']['x']
+        rechitsY     = dataset['rechits']['y']
         rechitsE     = dataset['rechits']['energy']
 
         for i in range(batchSize):