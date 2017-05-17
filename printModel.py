05df8ed374b333acb7cc7b3912e1cbc8e9f64a38
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-05-17 14:32:05 +0200

    initial commit: tool taking the pickled network model file and produces graphiz or image output

---
+++ printModel.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+
+#----------------------------------------------------------------------
+# main
+#----------------------------------------------------------------------
+
+ARGV = sys.argv[1:]
+
+assert len(ARGV) == 2
+
+inputFile, outputFile = ARGV
+
+
+# try to infer output format
+if outputFile.lower().endswith(".pdf"):
+    outputFormat = "pdf"
+elif outputFile.lower().endswith(".png"):
+    outputFormat = "png"
+elif outputFile.lower().endswith(".gv"):
+    outptuFormat = "raw"
+else:
+    print >> sys.stderr,"can't infer output format from output file",outputFile
+    sys.exit(1)
+
+#----------------------------------------
+
+import lasagne.layers
+
+# load the network
+import cPickle as pickle
+
+model = pickle.load(open(inputFile))['model']
+
+import draw_net
+dot = draw_net.get_pydot_graph(lasagne.layers.get_all_layers(model), verbose = True)
+
+dot.write(outputFile, format = outputFormat)
+
